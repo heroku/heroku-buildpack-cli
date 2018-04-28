87ff5a963a9d5e45b42063eabfc97fc6da999ddb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-27 18:43:54 -0700

    use v7 CLI

--- bin/heroku-cli.sh
+++ bin/heroku-cli.sh
@@ -3,5 +3,5 @@
 # Add Heroku CLI to PATH
 export PATH=$PATH:/app/heroku-cli/bin
 
-export HEROKU_SKIP_ANALYTICS=true
-export HEROKU_SKIP_CORE_UPDATES=true
+export HEROKU_SKIP_ANALYTICS=1
+export HEROKU_DISABLE_AUTOUPDATE=1
